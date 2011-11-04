c4c024f0d3fbd95d2840496389e2cf763d3c3cc5
Timothy Andrew <mail@timothyandrew.net>
2011-11-04 16:39:14 +0530

    renamed api_spec to server_spec

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,8 +1,7 @@
 require 'net/http'
-require 'instadroid-server'
 
-describe "apache" do
-  it "must be running on port 80" do
+describe "server" do
+  it "should be running on port 80" do
     expect { Net::HTTP.get "127.0.0.1", "/index.html", 80 }.
       to_not raise_error(Errno::ECONNREFUSED)
   end
